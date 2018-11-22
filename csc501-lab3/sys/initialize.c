b51af745db3d0a2d335ea08a2d5d078e7dc612ee
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-22 15:51:41 -0500

    reset

--- csc501-lab3/sys/initialize.c
+++ csc501-lab3/sys/initialize.c
@@ -7,7 +7,6 @@
 #include <bufpool.h>
 #include <proc.h>
 #include <sem.h>
-#include <lock.h>
 #include <sleep.h>
 #include <mem.h>
 #include <tty.h>
@@ -20,35 +19,35 @@
 #define	HOLESTART	(640 * 1024)
 #define	HOLEEND		((1024 + HOLESIZE) * 1024)  
 
-extern int main(); /* address of user's main prog	*/
-extern int start();
-extern int enable();
-extern int pci_init();
-extern int mon_init();
-extern int ripinit();
-LOCAL int sysinit();
+extern	int	main();			/* address of user's main prog	*/
+extern	int	start();
+extern	int	enable();
+extern	int	pci_init();
+extern	int	mon_init();
+extern	int	ripinit();
+LOCAL   int	sysinit();
 
 /* Declarations of major kernel variables */
-struct pentry proctab[NPROC]; /* process table			*/
-int nextproc; /* next process slot to use in create	*/
-struct sentry semaph[NSEM]; /* semaphore table			*/
-int nextsem; /* next sempahore slot to use in screate*/
-struct qent q[NQENT]; /* q table (see queue.c)		*/
-int nextqueue; /* next slot in q structure to use	*/
-char *maxaddr; /* max memory address (set by sizmem)	*/
-struct mblock memlist; /* list of free memory blocks		*/
+struct	pentry	proctab[NPROC]; /* process table			*/
+int	nextproc;		/* next process slot to use in create	*/
+struct	sentry	semaph[NSEM];	/* semaphore table			*/
+int	nextsem;		/* next sempahore slot to use in screate*/
+struct	qent	q[NQENT];	/* q table (see queue.c)		*/
+int	nextqueue;		/* next slot in q structure to use	*/
+char	*maxaddr;		/* max memory address (set by sizmem)	*/
+struct	mblock	memlist;	/* list of free memory blocks		*/
 #ifdef	Ntty
-struct tty tty[Ntty]; /* SLU buffers and mode control		*/
+struct  tty     tty[Ntty];	/* SLU buffers and mode control		*/
 #endif
 
 /* active system status */
-int numproc; /* number of live user processes	*/
-int currpid; /* id of currently running process	*/
-int reboot = 0; /* non-zero after first boot		*/
-int console_dev; /* console device			*/
+int	numproc;		/* number of live user processes	*/
+int	currpid;		/* id of currently running process	*/
+int	reboot = 0;		/* non-zero after first boot		*/
+int	console_dev;		/* console device			*/
 
-int rdyhead, rdytail; /* head/tail of ready list (q indicies)	*/
-char vers[100]; /* Xinu version printed at startup	*/
+int	rdyhead, rdytail;	/* head/tail of ready list (q indicies)	*/
+char	vers[100];		/* Xinu version printed at startup	*/
 
 /************************************************************************/
 /***				NOTE:				      ***/
@@ -68,14 +67,14 @@ char vers[100]; /* Xinu version printed at startup	*/
  *  nulluser  -- initialize system and become the null process (id==0)
  *------------------------------------------------------------------------
  */
-int nulluser() /* babysit CPU when no one home */
+int nulluser()				/* babysit CPU when no one home */
 {
 //	int userpid;
 
 	console_dev = SERIAL0;
 
 	initevec();
-
+	
 	sysinit();
 
 	sprintf(vers, "Xinu Version %s", VERSION);
@@ -85,14 +84,16 @@ int nulluser() /* babysit CPU when no one home */
 	else
 		kprintf("(reboot #%d)\n", reboot);
 
-	kprintf("%d bytes real mem\n", (unsigned long) maxaddr + 1);
+
+	kprintf("%d bytes real mem\n",
+		(unsigned long) maxaddr+1);
 #ifdef DETAIL	
 	kprintf("    %d", (unsigned long) 0);
 	kprintf(" to %d\n", (unsigned long) (maxaddr) );
 #endif	
 
 	kprintf("%d bytes Xinu code\n",
-			(unsigned long) ((unsigned long) &end - (unsigned long) start));
+		(unsigned long) ((unsigned long) &end - (unsigned long) start));
 #ifdef DETAIL	
 	kprintf("    %d", (unsigned long) start);
 	kprintf(" to %d\n", (unsigned long) &end );
@@ -100,18 +101,18 @@ int nulluser() /* babysit CPU when no one home */
 
 #ifdef DETAIL	
 	kprintf("%d bytes user stack/heap space\n",
-			(unsigned long) ((unsigned long) maxaddr - (unsigned long) &end));
+		(unsigned long) ((unsigned long) maxaddr - (unsigned long) &end));
 	kprintf("    %d", (unsigned long) &end);
 	kprintf(" to %d\n", (unsigned long) maxaddr);
 #endif	
-
-	kprintf("clock %sabled\n", clkruns == 1 ? "en" : "dis");
-	enable(); /* enable interrupts */
+	
+	kprintf("clock %sabled\n", clkruns == 1?"en":"dis");
+	enable();		/* enable interrupts */
 
 	open(CONSOLE, console_dev, 0);
 
 	/* create a process to execute the user's main program */
-	resume(create((int *) main, INITSTK, INITPRIO, INITNAME, INITARGS));
+	resume(create((int *)main,INITSTK,INITPRIO,INITNAME,INITARGS));
 
 	while (TRUE)
 		/* empty */;
@@ -121,95 +122,99 @@ int nulluser() /* babysit CPU when no one home */
  *  sysinit  --  initialize all Xinu data structeres and devices
  *------------------------------------------------------------------------
  */
-LOCAL int sysinit() {
-	int i, j;
-	struct pentry *pptr;
-	struct sentry *sptr;
-	struct mblock *mptr;
+LOCAL int sysinit()
+{
+	int	i,j;
+	struct	pentry	*pptr;
+	struct	sentry	*sptr;
+	struct	mblock	*mptr;
 
-	numproc = 0; /* initialize system variables */
-	nextproc = NPROC - 1;
-	nextsem = NSEM - 1;
-	nextqueue = NPROC; /* q[0..NPROC-1] are processes */
+	numproc = 0;			/* initialize system variables */
+	nextproc = NPROC-1;
+	nextsem = NSEM-1;
+	nextqueue = NPROC;		/* q[0..NPROC-1] are processes */
 
 	/* initialize free memory list */
 	/* PC version has to pre-allocate 640K-1024K "hole" */
-	if (maxaddr + 1 > (char *) HOLESTART) {
+	if (maxaddr+1 > (char *)HOLESTART) {
 		memlist.mnext = mptr = (struct mblock *) roundmb(&end);
-		mptr->mnext = (struct mblock *) HOLEEND;
-		mptr->mlen = (int) truncew(((unsigned) HOLESTART - (unsigned)&end));
-		mptr->mlen -= 4;
+		mptr->mnext = (struct mblock *)HOLEEND;
+		mptr->mlen = (int) truncew(((unsigned) HOLESTART -
+	     		 (unsigned)&end));
+        mptr->mlen -= 4;
 
 		mptr = (struct mblock *) HOLEEND;
 		mptr->mnext = 0;
-		mptr->mlen = (int) truncew((unsigned)maxaddr - HOLEEND - NULLSTK);
+		mptr->mlen = (int) truncew((unsigned)maxaddr - HOLEEND -
+	      		NULLSTK);
 	} else {
 		/* initialize free memory list */
 		memlist.mnext = mptr = (struct mblock *) roundmb(&end);
 		mptr->mnext = 0;
-		mptr->mlen = (int) truncew((unsigned)maxaddr - (int)&end - NULLSTK);
+		mptr->mlen = (int) truncew((unsigned)maxaddr - (int)&end -
+			NULLSTK);
 	}
+	
 
-	for (i = 0; i < NPROC; i++) /* initialize process table */
+	for (i=0 ; i<NPROC ; i++)	/* initialize process table */
 		proctab[i].pstate = PRFREE;
 
-	pptr = &proctab[NULLPROC]; /* initialize null process entry */
+	pptr = &proctab[NULLPROC];	/* initialize null process entry */
 	pptr->pstate = PRCURR;
-	for (j = 0; j < 7; j++)
+	for (j=0; j<7; j++)
 		pptr->pname[j] = "prnull"[j];
-	pptr->plimit = (WORD) (maxaddr + 1) - NULLSTK;
+	pptr->plimit = (WORD)(maxaddr + 1) - NULLSTK;
 	pptr->pbase = (WORD) maxaddr - 3;
-	pptr->pesp = pptr->pbase - 4; /* for stkchk; rewritten before used */
-	*((int *) pptr->pbase) = MAGIC;
+	pptr->pesp = pptr->pbase-4;	/* for stkchk; rewritten before used */
+	*( (int *)pptr->pbase ) = MAGIC;
 	pptr->paddr = (WORD) nulluser;
 	pptr->pargs = 0;
 	pptr->pprio = 0;
 	currpid = NULLPROC;
 
-
-	linit(); //Initialize locks
-
-	for (i = 0; i < NSEM; i++) { /* initialize semaphores */
+	for (i=0 ; i<NSEM ; i++) {	/* initialize semaphores */
 		(sptr = &semaph[i])->sstate = SFREE;
 		sptr->sqtail = 1 + (sptr->sqhead = newqueue());
 	}
 
-	rdytail = 1 + (rdyhead = newqueue());/* initialize ready list */
-
+	rdytail = 1 + (rdyhead=newqueue());/* initialize ready list */
 
 #ifdef	MEMMARK
-	_mkinit(); /* initialize memory marking */
+	_mkinit();			/* initialize memory marking */
 #endif
 
 #ifdef	RTCLOCK
-	clkinit(); /* initialize r.t.clock	*/
+	clkinit();			/* initialize r.t.clock	*/
 #endif
 
-	pci_init(); /* PCI */
+	pci_init();	/* PCI */
 
-	mon_init(); /* init monitor */
+	mon_init();	/* init monitor */
 //	ripinit();
 
 #ifdef NDEVS
-	for (i=0; i<NDEVS; i++ ) {
-		init_dev(i);
+	for (i=0 ; i<NDEVS ; i++ ) {	    
+	    init_dev(i);
 	}
 #endif
 
-	return (OK);
+	return(OK);
 }
 
-int stop(char *s) {
+int stop(char *s)
+{
 	kprintf("%s\n", s);
 	kprintf("looping... press reset\n");
-	while (1)
+	while(1)
 		/* empty */;
 }
 
-int delay(int n) {
+int delay(int n)
+{
 	DELAY(n);
-	return (OK);
+	return(OK);
 }
+
 
 #define	NBPG	4096
 
@@ -217,11 +222,12 @@ int delay(int n) {
  * sizmem - return memory size (in pages)
  *------------------------------------------------------------------------
  */
-long sizmem() {
-	unsigned char *ptr, *start, stmp, tmp;
-	int npages;
+long sizmem()
+{
+	unsigned char	*ptr, *start, stmp, tmp;
+	int		npages;
 
-	return 4096;
+	return 4096; 
 
 	start = ptr = 0;
 	npages = 0;
@@ -234,9 +240,9 @@ long sizmem() {
 		*ptr = tmp;
 		++npages;
 		ptr += NBPG;
-		if ((int) ptr == HOLESTART) { /* skip I/O pages */
-			npages += (1024 - 640) / 4;
-			ptr = (unsigned char *) HOLEEND;
+		if ((int)ptr == HOLESTART) {	/* skip I/O pages */
+			npages += (1024-640)/4;
+			ptr = (unsigned char *)HOLEEND;
 		}
 	}
 	return npages;
--- csc501-lab3/sys/kill.c
+++ csc501-lab3/sys/kill.c
@@ -8,66 +8,54 @@
 #include <io.h>
 #include <q.h>
 #include <stdio.h>
-#include <lock.h>
 
 /*------------------------------------------------------------------------
  * kill  --  kill a process and remove it from the system
  *------------------------------------------------------------------------
  */
-SYSCALL kill(int pid) {
-	STATWORD ps;
-	struct pentry *pptr; /* points to proc. table for pid*/
-	int dev,i;
+SYSCALL kill(int pid)
+{
+	STATWORD ps;    
+	struct	pentry	*pptr;		/* points to proc. table for pid*/
+	int	dev;
 
 	disable(ps);
-	if (isbadpid(pid) || (pptr = &proctab[pid])->pstate == PRFREE) {
+	if (isbadpid(pid) || (pptr= &proctab[pid])->pstate==PRFREE) {
 		restore(ps);
-		return (SYSERR);
+		return(SYSERR);
 	}
 	if (--numproc == 0)
 		xdone();
 
 	dev = pptr->pdevs[0];
-	if (!isbaddev(dev))
+	if (! isbaddev(dev) )
 		close(dev);
 	dev = pptr->pdevs[1];
-	if (!isbaddev(dev))
+	if (! isbaddev(dev) )
 		close(dev);
 	dev = pptr->ppagedev;
-	if (!isbaddev(dev))
+	if (! isbaddev(dev) )
 		close(dev);
-
+	
 	send(pptr->pnxtkin, pid);
 
 	freestk(pptr->pbase, pptr->pstklen);
 	switch (pptr->pstate) {
 
-	case PRCURR:
-		pptr->pstate = PRFREE; /* suicide */
-		for (i = 0; i < NLOCKS; i++) {
-				release(pid, locks[i].lrefNum);
-		}
-		resched();
+	case PRCURR:	pptr->pstate = PRFREE;	/* suicide */
+			resched();
 
-	case PRWAIT:
-		semaph[pptr->psem].semcnt++;
+	case PRWAIT:	semaph[pptr->psem].semcnt++;
 
-	case PRREADY:
-		for (i = 0; i < NLOCKS; i++) {
-				release(pid, locks[i].lrefNum);
-		}
-		dequeue(pid);
-		pptr->pstate = PRFREE;
-		break;
+	case PRREADY:	dequeue(pid);
+			pptr->pstate = PRFREE;
+			break;
 
 	case PRSLEEP:
-	case PRTRECV:
-		unsleep(pid);
-		/* fall through	*/
-	default:
-		pptr->pstate = PRFREE;
+	case PRTRECV:	unsleep(pid);
+						/* fall through	*/
+	default:	pptr->pstate = PRFREE;
 	}
-
 	restore(ps);
-	return (OK);
+	return(OK);
 }
