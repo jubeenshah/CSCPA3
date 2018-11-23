0d1fdd03c97716a3b37a7f5dca176504a5d76d87
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-23 10:57:13 -0500

    Update chprio.c

--- csc501-lab3/sys/chprio.c
+++ csc501-lab3/sys/chprio.c
@@ -6,6 +6,9 @@
 #include <q.h>
 #include <stdio.h>
 
+#define SETONE 	1
+#define SETZERO	0
+
 /*------------------------------------------------------------------------
  * chprio  --  change the scheduling priority of a process
  *------------------------------------------------------------------------
@@ -16,17 +19,55 @@ SYSCALL chprio(int pid, int newprio)
 	struct	pentry	*pptr;
 
 	disable(ps);
-	if (isbadpid(pid) || newprio<=0 ||
-	    (pptr = &proctab[pid])->pstate == PRFREE) {
+	pptr = &proctab[pid];
+	int checkState = pptr->pstate;
+	if (isbadpid(pid) || newprio<=SETZERO ||
+	    checkState == PRFREE) {
 		restore(ps);
 		return(SYSERR);
 	}
 	pptr->pprio = newprio;
 	newpinh(pid);
 
-	if(pptr->pstate== PRLOCK){
-		newlprio(pptr->lockid);
+	int checkState = pptr->pstate;
+
+	if(checkState == PRLOCK) {
+		int setLockID = pptr->lockid;
+		newlprio(setLockID);
 	}
 	restore(ps);
 	return(newprio);
 }
+
+// /* chprio.c - chprio */
+//
+// #include <conf.h>
+// #include <kernel.h>
+// #include <proc.h>
+// #include <q.h>
+// #include <stdio.h>
+//
+// /*------------------------------------------------------------------------
+//  * chprio  --  change the scheduling priority of a process
+//  *------------------------------------------------------------------------
+//  */
+// SYSCALL chprio(int pid, int newprio)
+// {
+// 	STATWORD ps;
+// 	struct	pentry	*pptr;
+//
+// 	disable(ps);
+// 	if (isbadpid(pid) || newprio<=0 ||
+// 	    (pptr = &proctab[pid])->pstate == PRFREE) {
+// 		restore(ps);
+// 		return(SYSERR);
+// 	}
+// 	pptr->pprio = newprio;
+// 	newpinh(pid);
+//
+// 	if(pptr->pstate== PRLOCK){
+// 		newlprio(pptr->lockid);
+// 	}
+// 	restore(ps);
+// 	return(newprio);
+// }
