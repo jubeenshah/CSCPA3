1c524e75c7644223a963cc41f0c56641d173c631
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-23 09:30:30 -0500

    Update linit.c

--- csc501-lab3/sys/linit.c
+++ csc501-lab3/sys/linit.c
@@ -13,18 +13,25 @@ struct lentry locks[NLOCKS];
 int nextlock, lockaround;
 void linit(){
 	register struct lentry *lptr;
-	lockaround=SETZERO;
-	nextlock=NLOCKS-SETONE;
+	lockaround = SETZERO;
+  int setNextLock = NLOCKS-SETONE;
+	nextlock   = setNextLock ;
 	int i,j;
+
 	for(i=SETZERO;i<NLOCKS;++i){
 		lptr=&locks[i];
 		lptr->lstate=LFREE;
 		lptr->lqtail=SETONE+(lptr->lqhead= newqueue());
 		lptr->lprio=SETZERO;
 
-		for(j=SETZERO;j<NPROC;++j){
-			lptr->pidheld[j]=SETZERO;
-		}
-
+		// for(j=SETZERO;j<NPROC;++j){
+		// 	lptr->pidheld[j]=SETZERO;
+		// }
+    j = SETZERO;
+    while (j < NPROC) {
+      /* code */
+      lptr->pidheld[j]=SETZERO;
+      j = j + SETONE;
+    }
 	}
 }
