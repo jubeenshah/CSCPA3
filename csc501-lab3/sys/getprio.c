d630dfec60dde9f489517a223b9d551aca080cb6
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-23 10:48:41 -0500

    Update getprio.c

--- csc501-lab3/sys/getprio.c
+++ csc501-lab3/sys/getprio.c
@@ -5,6 +5,7 @@
 #include <proc.h>
 #include <stdio.h>
 
+#define SETZERO 	0
 /*------------------------------------------------------------------------
  * getprio -- return the scheduling priority of a given process
  *------------------------------------------------------------------------
@@ -20,5 +21,35 @@ SYSCALL getprio(int pid)
 		return(SYSERR);
 	}
 	restore(ps);
-	return(pptr->pinh==0?pptr->pprio:pptr->pinh);
+	if (pptr->pinh == SETZERO) {
+		/* code */
+		return(pptr->pprio);
+	}
+	return(pptr->pinh);
+
 }
+
+// /* getprio.c - getprio */
+//
+// #include <conf.h>
+// #include <kernel.h>
+// #include <proc.h>
+// #include <stdio.h>
+//
+// /*------------------------------------------------------------------------
+//  * getprio -- return the scheduling priority of a given process
+//  *------------------------------------------------------------------------
+//  */
+// SYSCALL getprio(int pid)
+// {
+// 	STATWORD ps;
+// 	struct	pentry	*pptr;
+//
+// 	disable(ps);
+// 	if (isbadpid(pid) || (pptr = &proctab[pid])->pstate == PRFREE) {
+// 		restore(ps);
+// 		return(SYSERR);
+// 	}
+// 	restore(ps);
+// 	return(pptr->pinh==0?pptr->pprio:pptr->pinh);
+// }
