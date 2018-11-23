286e58485dd1411dcb6d4c52d5f3e46944ad3625
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-23 09:27:03 -0500

    Update linit.c

--- csc501-lab3/sys/linit.c
+++ csc501-lab3/sys/linit.c
@@ -1,67 +1,67 @@
-#include <conf.h>
-#include <kernel.h>
-#include "lock.h"
-#include <proc.h>
-#include <q.h>
-#include <stdio.h>
-
-#define SETONE  1
-#define SETZERO 0
-
-struct lentry locks[NLOCKS];
-int nextlock, lockaround;
-
-void linit() {
-  register struct lentry *lptr;
-  lockaround    = SETZERO;
-  nextlock      = NLOCKS;
-  int index     = SETZERO;
-  int indexDos  = SETZERO;
-
-  while (index < NLOCKS) {
-    /* code */
-    lptr=&locks[index];
-    lptr->lstate  = SETONE + (lptr->lqhead=newqueue());
-    lptr->lprio   = SETZERO;
-    index = index + 1;
-  }
-
-  while (indexDos < NPROC) {
-    /* code */
-    lptr->pidheld[indexDos] = SETZERO;
-    indexDos = indexDos + 1;
-  }
-}
-
-// /* linit.c - linit() */
+// #include <conf.h>
+// #include <kernel.h>
+// #include "lock.h"
+// #include <proc.h>
+// #include <q.h>
+// #include <stdio.h>
 //
-// #include<conf.h>
-// #include<kernel.h>
-// #include<proc.h>
-// #include<q.h>
-// #include<lock.h>
-// #include<stdio.h>
+// #define SETONE  1
+// #define SETZERO 0
 //
 // struct lentry locks[NLOCKS];
-// int nextlock;
+// int nextlock, lockaround;
 //
-// int lockaround;
+// void linit() {
+//   register struct lentry *lptr;
+//   lockaround    = SETZERO;
+//   nextlock      = NLOCKS;
+//   int index     = SETZERO;
+//   int indexDos  = SETZERO;
 //
-// /* call in initialize.c */
-// void linit(){
-// 	register struct lentry *lptr;
-// 	lockaround=0;
-// 	nextlock=NLOCKS-1;
-// 	int i,j;
-// 	for(i=0;i<NLOCKS;++i){
-// 		lptr=&locks[i];
-// 		lptr->lstate=LFREE;
-// 		lptr->lqtail=1+(lptr->lqhead= newqueue());
-// 		lptr->lprio=0;
+//   while (index < NLOCKS) {
+//     /* code */
+//     lptr=&locks[index];
+//     lptr->lstate  = SETONE + (lptr->lqhead=newqueue());
+//     lptr->lprio   = SETZERO;
+//     index = index + 1;
+//   }
 //
-// 		for(j=0;j<NPROC;++j){
-// 			lptr->pidheld[j]=0;
-// 		}
-//
-// 	}
+//   while (indexDos < NPROC) {
+//     /* code */
+//     lptr->pidheld[indexDos] = SETZERO;
+//     indexDos = indexDos + 1;
+//   }
 // }
+
+/* linit.c - linit() */
+
+#include<conf.h>
+#include<kernel.h>
+#include<proc.h>
+#include<q.h>
+#include<lock.h>
+#include<stdio.h>
+
+struct lentry locks[NLOCKS];
+int nextlock;
+
+int lockaround;
+
+/* call in initialize.c */
+void linit(){
+	register struct lentry *lptr;
+	lockaround=0;
+	nextlock=NLOCKS-1;
+	int i,j;
+	for(i=0;i<NLOCKS;++i){
+		lptr=&locks[i];
+		lptr->lstate=LFREE;
+		lptr->lqtail=1+(lptr->lqhead= newqueue());
+		lptr->lprio=0;
+
+		for(j=0;j<NPROC;++j){
+			lptr->pidheld[j]=0;
+		}
+
+	}
+}
