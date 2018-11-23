c149500f244c27606d24c1f0f6878860d86507c5
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-23 10:34:11 -0500

    Update kill.c

--- csc501-lab3/sys/kill.c
+++ csc501-lab3/sys/kill.c
@@ -8,6 +8,7 @@
 #include <io.h>
 #include <q.h>
 #include <stdio.h>
+
 #include "lock.h"
 
 #define SETONE 	1
@@ -18,6 +19,7 @@
  */
 SYSCALL kill(int pid)
 {
+	int i;
 	STATWORD ps;
 	struct	pentry	*pptr;		/* points to proc. table for pid*/
 	int	dev;
@@ -27,10 +29,10 @@ SYSCALL kill(int pid)
 		restore(ps);
 		return(SYSERR);
 	}
-	if (--numproc == 0)
+	if (--numproc == SETZERO)
 		xdone();
 
-	dev = pptr->pdevs[0];
+	dev = pptr->pdevs[SETZERO];
 	if (! isbaddev(dev) )
 		close(dev);
 	dev = pptr->pdevs[1];
@@ -53,106 +55,23 @@ SYSCALL kill(int pid)
 	case PRREADY:	dequeue(pid);
 			pptr->pstate = PRFREE;
 			break;
+	case PRLOCK:
+				dequeue(pid);
+				locks[pptr->lockid].pidheld[pid]=SETZERO;
+				newlprio(pptr->lockid);
+				for(i=SETZERO;i<NPROC;++i){
+					if(locks[pptr->lockid].pidheld[i]==SETONE){
+						newpinh(i);
+					}
+				}
+				pptr->pstate=PRFREE;
+				break;
 
 	case PRSLEEP:
 	case PRTRECV:	unsleep(pid);
 						/* fall through	*/
-	case PRLOCK:
-				dequeue(pid);
-				int getLockID = pptr->lockid;
-				locks[getLockID].pidheld[pid] = SETZERO;
-				newlprio(getLockID);
-				int index = SETZERO;
-				while (index < 30) {
-					/* code */
-					int getLockIDAgain = pptr->lockid;
-					int checkVal = locks[getLockIDAgain].pidheld[index];
-					if (checkVal == SETONE) {
-						/* code */
-						newpinh(index);
-					}
-					index = index + SETONE;
-				}
-				pptr->pstate = PRFREE;
-				break;
-
 	default:	pptr->pstate = PRFREE;
 	}
 	restore(ps);
 	return(OK);
 }
-
-// /* kill.c - kill */
-//
-// #include <conf.h>
-// #include <kernel.h>
-// #include <proc.h>
-// #include <sem.h>
-// #include <mem.h>
-// #include <io.h>
-// #include <q.h>
-// #include <stdio.h>
-//
-// #include <lock.h>
-// /*------------------------------------------------------------------------
-//  * kill  --  kill a process and remove it from the system
-//  *------------------------------------------------------------------------
-//  */
-// SYSCALL kill(int pid)
-// {
-// 	int i;
-// 	STATWORD ps;
-// 	struct	pentry	*pptr;		/* points to proc. table for pid*/
-// 	int	dev;
-//
-// 	disable(ps);
-// 	if (isbadpid(pid) || (pptr= &proctab[pid])->pstate==PRFREE) {
-// 		restore(ps);
-// 		return(SYSERR);
-// 	}
-// 	if (--numproc == 0)
-// 		xdone();
-//
-// 	dev = pptr->pdevs[0];
-// 	if (! isbaddev(dev) )
-// 		close(dev);
-// 	dev = pptr->pdevs[1];
-// 	if (! isbaddev(dev) )
-// 		close(dev);
-// 	dev = pptr->ppagedev;
-// 	if (! isbaddev(dev) )
-// 		close(dev);
-//
-// 	send(pptr->pnxtkin, pid);
-//
-// 	freestk(pptr->pbase, pptr->pstklen);
-// 	switch (pptr->pstate) {
-//
-// 	case PRCURR:	pptr->pstate = PRFREE;	/* suicide */
-// 			resched();
-//
-// 	case PRWAIT:	semaph[pptr->psem].semcnt++;
-//
-// 	case PRREADY:	dequeue(pid);
-// 			pptr->pstate = PRFREE;
-// 			break;
-// 	case PRLOCK:
-// 				dequeue(pid);
-// 				locks[pptr->lockid].pidheld[pid]=0;
-// 				newlprio(pptr->lockid);
-// 				for(i=0;i<NPROC;++i){
-// 					if(locks[pptr->lockid].pidheld[i]==1){
-// 						newpinh(i);
-// 					}
-// 				}
-// 				pptr->pstate=PRFREE;
-// 				break;
-//
-// 	case PRSLEEP:
-// 	case PRTRECV:	unsleep(pid);
-// 						/* fall through	*/
-// 	default:	pptr->pstate = PRFREE;
-// 	}
-// 	restore(ps);
-// 	return(OK);
-// }
