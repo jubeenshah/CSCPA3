1a2c0b9d5e7154486d82fe93d48698cd40cbc946
Jubeen Shah <jnshah2@ncsu.edu>
2018-11-23 09:41:03 -0500

    Update ldelete.c

--- csc501-lab3/sys/ldelete.c
+++ csc501-lab3/sys/ldelete.c
@@ -34,59 +34,19 @@ switch (ret) {
 }
 
 lptr=&locks[lock];
-lptr->lstate = LDELETED;
+lptr->lstate = '\03';
 
 int emptyIS = lptr->lqhead;
 if (nonempty(emptyIS)) {
   /* code */
   int emptyISAgain = lptr->lqhead;
   pid = getfirst(emptyISAgain);
-  while (pid != EMPTY ) {
+  while (pid != (-1)) ) {
     /* code */
-    ready(pid, RESCHNO);
+    ready(pid, 0);
   }
   resched();
 }
 restore(ps);
-return(OK);
+return(1);
 }
-
-// /* ldelete.c - ldelete */
-//
-// #include<conf.h>
-// #include<kernel.h>
-// #include<proc.h>
-// #include<q.h>
-// #include<lock.h>
-// #include<stdio.h>
-//
-// SYSCALL ldelete(int lockdescriptor){
-// 	STATWORD ps;
-// 	struct lentry *lptr;
-//
-// 	int pid;
-// 	int lock=lockdescriptor/LOCKMAXAROUND;
-//
-// 	disable(ps);
-// 	int ret=lock_err(lockdescriptor);
-// 	if(ret==SYSERR||ret==DELETED){
-// 		restore(ps);
-// 		return (ret);
-// 	}
-//
-// 	lptr=&locks[lock];
-// 	lptr->lstate=LDELETED;
-//
-//
-// /*awakens all the waiting processes by moving them from the lock queue to the ready list.*/
-// 	if(nonempty(lptr->lqhead)){
-// 		while((pid=getfirst(lptr->lqhead))!=EMPTY){
-// 			proctab[pid].plockret=DELETED;
-// 			ready(pid,RESCHNO);
-// 		}
-// 		resched();
-// 	}
-// 	restore(ps);
-// 	return (OK);
-// }
-//
